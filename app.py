90fdefabf4f6cc87c79ac73e3d34163b502d0cde
Yasaswini-ch <134924003+Yasaswini-ch@users.noreply.github.com>
2025-05-14 18:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ uploaded_file = st.file_uploader("Upload an image of a forest scene", type=["jpg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     # st.markdown("---") # Optional: Can keep for visual separation or remove
     # st.subheader("Preprocessing Details (for debugging):") # REMOVE/COMMENT OUT
